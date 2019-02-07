f848da7bddcb7424a20935070062767d491a726d
Joaquin Silvestre <juaco.silvestre@gmail.com>
2019-02-07 15:26:31 -0300

    second commit

--- src/main/java/com/organization/bootchampweb/dao/UserRepository.java
+++ src/main/java/com/organization/bootchampweb/dao/UserRepository.java
@@ -1,6 +1,7 @@
 package com.organization.bootchampweb.dao;
 
 import com.organization.bootchampweb.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,15 @@ import org.springframework.stereotype.Repository;
 
 // CRUD refers Create, Read, Update, Delete
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer>, JpaRepository<User, Integer> {
 
     User findById(Long id);
+
+    User findByEmail(String email);
+
+    User findByUserName(String useraName);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUserName(String email);
 }
--- src/main/java/com/organization/bootchampweb/model/User.java
+++ src/main/java/com/organization/bootchampweb/model/User.java
@@ -1,47 +1,56 @@
 package com.organization.bootchampweb.model;
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Collection;
 
+@Data
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
+
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
-    @NotBlank
+    @NotNull(message = "Name cannot be null")
     private String name;
-    @NotBlank
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "invalid email format")
     private String email;
-    @NotBlank
-    private Integer dni;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @NotNull(message = "Username cannot be null")
+    @Size(min = 5, max = 50, message
+            = "About Me must be between 5 and 50 characters")
+    private String userName;
+    @NotNull(message = "Password cannot be null")
+    private String password;
 
     public String getEmail() {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public Integer getDni() { return dni; }
+    public String getUserName() {
+        return userName;
+    }
 
-    public void setDni(Integer dni) { this.dni = dni; }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
 }
